b9c3ec8bb9bb095fe6ca8a3bd4988208857d91c7
hmatheis <134367249+hmatheis@users.noreply.github.com>
2025-05-13 14:56:12 +0200

    Update clean.py

--- scripts/clean.py
+++ scripts/clean.py
@@ -78,7 +78,7 @@ def clean(df_training,df_test,num_features):
         
         
     np_train_prepared = full_pipeline.fit_transform(df_training)
-    np_test_prepared = full_pipeline.transform(df_test)
+    np_test_prepared = full_pipeline.fit_transform(df_test)
     print(np_train_prepared[:2])
     print(np.isnan(np.min(np_train_prepared)), np.isnan(np.min(np_test_prepared)))
     print(full_pipeline.transformers_[1][1].steps[1][1].get_feature_names_out()[10] )#16 toluol low
